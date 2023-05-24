7969eb9e2c409e1f0b12a4f25990ac1ce9695519
Minavar_Cydeo <mine.hesenova.96@gmail.com>
2023-05-23 23:27:32 -0400

    Created divisionWithoutOperator class

---
+++ src/week1/divisionWithoutOperator.java
@@ -0,0 +1,22 @@
+package week1;
+
+public class divisionWithoutOperator {
+    /*
+    2️⃣  Numbers -- Divide without / operator
+Write a method that can divide two numbers without using division operator.
+     */
+
+    public static void divide(int num1, int num2){
+        int count =0;
+
+        while(num1 >= num2){
+            num1 -= num2;
+            count++;
+        }
+        System.out.println(count + " and reminder is: " + num1);
+    }
+
+    public static void main(String[] args) {
+        divide(20,3);
+    }
+}
