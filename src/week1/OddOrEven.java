122f210c8794f8f50bdaafb072a5925a1b49bb4f
yuliyagorenkova <y.gorenkova@gmail.com>
2023-05-24 09:26:03 -0400

    created OddOrEven task

---
+++ src/week1/OddOrEven.java
@@ -0,0 +1,20 @@
+package week1;
+
+public class OddOrEven {
+    public int number;
+
+    public static void identify(int number){
+        if (number % 2==0){
+            System.out.println("Even");
+        }else {
+            System.out.println("Odd");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        identify(5);
+
+    }
+}
