934c19628becdc506825866955bc276902a67198
Saliyon4 <sali.abr.4@gmail.com>
2023-05-23 22:09:42 -0400

    created identify class

---
+++ src/Identify.java
@@ -0,0 +1,15 @@
+public class Identify {
+
+    public static void  identify(int number) {
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else if(number%2!=0) {
+            System.out.println("Odd");
+        }else{
+            System.out.println("Zero");
+        }
+
+    }
+}
+
+
