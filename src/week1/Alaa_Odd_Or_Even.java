539ec210741dbf2145122e1a66f2fd633cb77d98
Alaa Ahmed <ummoka@live.com>
2023-05-31 06:11:07 -0400

    updated Even or Odd by Alaa

---
+++ src/week1/Alaa_Odd_Or_Even.java
@@ -0,0 +1,22 @@
+package week1;
+import java.util.Scanner;
+public class Alaa_Odd_Or_Even {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        if (num % 2 ==0){
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+
+
+
+
+
+    }
+}
